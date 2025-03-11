75195de51b6a67b259d6856a48876b3d7ea25675
beanc16 <bean.christopher@outlook.com>
2025-03-11 14:37:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# vanilla-rest-client
+
+A vanilla, no-dependency client for sending REST requests.
+
+
+
+## Table of Contents
+- [vanilla-rest-client](#vanilla-rest-client)
+  - [Table of Contents](#table-of-contents)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [License](#license)
+
+
+## Install
+This is a [Node.js](https://nodejs.org/en/) module available through the [npm registry](https://www.npmjs.com/).
+
+```bash
+$ npm install vanilla-rest-client
+```
+
+
+
+## Usage
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ jest.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!node_modules/**',
+    ],
+    coveragePathIgnorePatterns: [
+    ],
+    coverageThreshold: {
+        './src': {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+        },
+    },
+    moduleFileExtensions: ['ts'],
+    extensionsToTreatAsEsm: ['.ts'],
+    resolver: 'ts-jest-resolver',
+    transform: {
+        '^.+\\.ts?$': [
+            '@swc/jest',
+        ],
+    },
+};
